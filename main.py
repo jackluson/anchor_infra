7855b22a544f29f06bdaf3325c7d6a938984c0ac
jackluson <luxuemin2108@gmail.com>
2023-06-24 20:14:49 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Desc:
+File: /main.py
+File Created: Saturday, 24th June 2023 6:10:04 pm
+Author: luxuemin2108@gmail.com
+-----
+Copyright (c) 2023 Camel Lu
+'''
+if __name__ == '__main__':
+    print('Module is being executed directly, so do stuff here')
