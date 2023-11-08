d97614ef3d5bdd0efdf29352f0fe0320b3622a8e
jackluson <luxuemin2108@gmail.com>
2023-11-09 01:28:53 +0800

    feat: update query_stock_main_financial

--- sql/stocks/query.py
+++ sql/stocks/query.py
@@ -55,11 +55,15 @@ LEFT JOIN stock_profile as t1 ON t.stock_code = t1.stock_code WHERE t.`stock_cod
         results = self.dict_cursor.fetchone()
         return results
 
-    def query_special_stock_main_financial(self, code, timestamp):
+    def query_stock_main_financial(self, *, code=None, report_date):
         """ 查看股票主要财务数据
         """
-        query_stock_sql = "SELECT b.price, b.pe_ttm, b.pe_lyr, b.pe_forecast, a.*, c.industry_name_third, c.industry_name_second, c.industry_name_first, b.goodwill_in_net_assets, b.market_capital FROM stock_main_financial_indicator as a LEFT JOIN stock_daily_info as b ON a.code = b.code LEFT JOIN stock_industry as c ON a.code = c.stock_code WHERE a.code = %s AND b.timestamp = %s"
-        self.dict_cursor.execute(query_stock_sql, [code, timestamp])
+        if code:
+            query_stock_sql = "SELECT a.* FROM stock_main_financial_indicator as a WHERE a.code = %s AND a.report_date = %s"
+            self.dict_cursor.execute(query_stock_sql, [code, report_date])
+        else:
+            query_stock_sql = "SELECT a.* FROM stock_main_financial_indicator as a WHERE a.report_date = %s"
+            self.dict_cursor.execute(query_stock_sql, [report_date])
 
         results = self.dict_cursor.fetchall()
         return results
