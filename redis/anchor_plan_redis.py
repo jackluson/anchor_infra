fa124862926657dbf4f6a69b20a067c1cbc4e6d2
jackluson <luxuemin2108@gmail.com>
2023-12-17 00:36:51 +0800

    feat: add get and set about convertible_bond

--- redis/anchor_plan_redis.py
+++ redis/anchor_plan_redis.py
@@ -1,22 +1,27 @@
-import json
 from infra.redis.base import BaseRedis
 
 _global_redis = None
-
 class AnchorPlanRedis(BaseRedis):
     def __init__(self):
         super().__init__()
         self.mongo_cur_holder_list_key = 'mongo#cur_holder_list'
         self.mongo_cur_care_list_key = 'mongo#cur_care_list'
-    def set_with_dumps(self, key, value, expire=2 * 60 * 60):
-        encoded_data = json.dumps(value)
-        self.client.set(key, encoded_data, 2 * 60 * 60)
-    def get_with_loads(self, key):
-        res = self.client.get(key)
-        if res:
-        # Retrieve the data from Redis and decode it using json.loads
-          decoded_data = json.loads(res)
-          return decoded_data
+
+        self.convertible_bond_filter_listed_all_key = 'convertible_bond#filter_listed_all'
+        self.convertible_bond_filter_listed_all_exclude_new = 'convertible_bond#filter_listed_all_exclude_new'
+        self.convertible_bond_filter_double_low_key = 'convertible_bond#filter_double_low'
+        self.convertible_bond_filter_multiple_factors_key = 'convertible_bond#filter_multiple_factors'
+        self.convertible_bond_filter_low_level_stock_key = 'convertible_bond#filter_low_level_stock'
+        self.convertible_bond_filter_genie_key = 'convertible_bond#filter_genie'
+        self.convertible_bond_filter_small_scale_not_ransom_key = 'convertible_bond#filter_small_scale_not_ransom'
+        self.convertible_bond_filter_new_small_key = 'convertible_bond#filter_new_small'
+        self.convertible_bond_filter_candidate_key = 'convertible_bond#filter_candidate'
+        self.convertible_bond_filter_downward_revise_key = 'convertible_bond#filter_downward_revise'
+        self.convertible_bond_filter_profit_due_key = 'convertible_bond#filter_profit_due'
+        self.convertible_bond_filter_return_lucky_key = 'convertible_bond#filter_return_lucky'
+
+        self.convertible_bond_stats_info_key = 'convertible_bond#stats_info'
+
     def set_cur_holder_list(self, holder_list, expire=2 * 60 * 60 ):
         return self.set_with_dumps(self.mongo_cur_holder_list_key, holder_list, expire)
     def get_cur_holder_list(self):
@@ -26,6 +31,30 @@ class AnchorPlanRedis(BaseRedis):
     def get_cur_care_list(self):
         return self.get_with_loads(self.mongo_cur_care_list_key)
 
+    def set_filter_listed_all(self, listed_all, expire=2 * 60 * 60 ):
+        return self.set_with_dumps(self.convertible_bond_filter_listed_all_key, listed_all, expire)
+
+    def get_filter_listed_all(self):
+        return self.get_with_loads(self.convertible_bond_filter_listed_all_key)
+
+    def set_convertible_bond_filter_result(self, key, value, *, full_key=None):
+        full_key = 'convertible_bond#' + key
+        persistence_seconds = self.get_expire_seconds_based_quote()
+        return self.set_with_dumps(full_key, value, expire=persistence_seconds)
+    def get_convertible_bond_filter_result(self, key=None, *, full_key=None):
+        full_key = full_key if full_key else 'convertible_bond#' + key
+        return self.get_with_loads(full_key)
+    
+    def set_convertible_bond_stats_info(self, title, value, *, full_key=None):
+        full_key = full_key if full_key else f'convertible_bond#stats_info_{title}'
+        persistence_seconds = self.get_expire_seconds_based_quote()
+        return self.hset(full_key, mapping=value, expire=persistence_seconds)
+    def get_convertible_bond_stats_info(self, title=None, *, full_key=None, field:str=None):
+        full_key = full_key if full_key else f'convertible_bond#stats_info_{title}'
+        if field:
+            return self.hget(full_key, field)
+        else:
+            return self.hgetall(full_key)
 
 def create_singleton_anchor_plan_redis():
     global _global_redis
@@ -35,4 +64,3 @@ def create_singleton_anchor_plan_redis():
   
 def get_anchor_plan_redis():
     return create_singleton_anchor_plan_redis()
-# def cache()
--- redis/base.py
+++ redis/base.py
@@ -1,11 +1,63 @@
 
+from datetime import datetime, timedelta
+import json
+import pytz
 import redis
 from infra.config.env import env_redis_db, env_redis_host, env_redis_port
 
 
+import decimal
+class DecimalEncoder(json.JSONEncoder): 
+    def default(self, o): 
+        if isinstance(o, decimal.Decimal): return str(o) 
+        return super().default(o)
+
 class BaseRedis(object):
     client:redis.Redis = None
     def __init__(self):
-        self.client = redis.Redis(host=env_redis_host, port=env_redis_port, db=env_redis_db)
+        self.client = redis.StrictRedis(host=env_redis_host, port=env_redis_port, db=env_redis_db, decode_responses=True)
 
+    def set(self, *arg, **kwargs):
+        return self.client.set(*arg, **kwargs)
+    def get(self, *arg, **kwargs):
+        return self.client.get(*arg, **kwargs)
 
+    def hset(self, *arg, **kwargs):
+        expire = kwargs.pop('expire', None)
+        mapping = kwargs.pop('mapping', None)
+        self.client.hset(*arg, mapping=mapping, **kwargs)
+        if expire:
+            self.client.expire(arg[0], expire)
+    def hget(self, *arg, **kwargs):
+        return self.client.hget(*arg, **kwargs)
+    def hgetall(self, *arg, **kwargs):
+        return self.client.hgetall(*arg, **kwargs)
+    
+    def set_with_dumps(self, key, value, expire=2 * 60 * 60):
+        encoded_data = json.dumps(value, cls=DecimalEncoder)
+        return self.set(key, encoded_data, expire)
+
+    def get_with_loads(self, key):
+        res = self.get(key)
+        if res:
+        # Retrieve the data from Redis and decode it using json.loads
+          decoded_data = json.loads(res)
+          return decoded_data
+
+    def get_expire_seconds_based_quote(self):
+        tz=pytz.timezone('Asia/Shanghai')
+        now = datetime.now(tz=tz)
+        # start_of_day = datetime(now.year, now.month, now.day, 0, 0, 0)
+        #如果是15点之前，那么过期时间就是今天15点，否则就是明天15点
+        timedelta_day = 1
+        weekday = now.weekday()
+        if now.hour < 15 and 0 <= weekday < 5:
+            expire_by = datetime(now.year, now.month, now.day, 15, 0, 0, tzinfo=tz)
+        else:
+            if weekday == 4:
+                timedelta_day = 3
+            elif weekday == 5:
+                timedelta_day = 2
+            expire_by = datetime(now.year, now.month, now.day, 15, 0, 0, tzinfo=tz) + timedelta(days=timedelta_day)
+        persistence_seconds = int((expire_by - now).total_seconds())
+        return persistence_seconds
