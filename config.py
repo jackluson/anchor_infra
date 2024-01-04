6acee1f9f9bf2693e7b91f8e4417154b62591c3d
jackluson <luxuemin2108@gmail.com>
2024-01-04 22:18:01 +0800

    feat: add config

---
+++ config.py
@@ -0,0 +1,20 @@
+'''
+Desc:
+File: /config..py
+File Created: Tuesday, 4th July 2023 12:31:01 am
+Author: luxuemin2108@gmail.com
+-----
+Copyright (c) 2023 Camel Lu
+'''
+
+POSITION_GROUP = [
+    '摊大饼-多低修',
+    '摊大饼-小波',
+    'ST-ETF-配',
+]
+
+CARE_GROUP = [
+    'pre',
+    'focus',
+    'trace'
+]
